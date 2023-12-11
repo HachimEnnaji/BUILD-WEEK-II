c9160cf985d01355b9a67f758bfd9624dbf36d92
HachimEnnaji <ashennaji@hotmail.it>
2023-12-11 11:17:53 +0100

    added sass and link cdn bootstrap

---
+++ js/token.js
@@ -0,0 +1,11 @@
+const URL = "https://deezerdevs-deezer.p.rapidapi.com/infos";
+const token = "8f76b272f2msh3aee6aa14119e3bp1a1f1bjsnf563f456d57b";
+
+const options = {
+  method: "GET",
+  url: URL,
+  headers: {
+    "X-RapidAPI-Key": token,
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+};

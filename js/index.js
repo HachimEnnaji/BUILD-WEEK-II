2b2e071144ab451cb7b9352cdfa14570a001716f
HachimEnnaji <ashennaji@hotmail.it>
2023-12-13 01:59:29 +0100

    added index file js to main

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 import { URL, token, options, search, playlist } from "./token.js";
 
+const arrArtist = [
+  "Eminem",
+  "Rihanna",
+  "The-Beatles",
+  "Taylor-Swift",
+  "Imagine-Dragons",
+  "Linkin-Park",
+  "Oasis",
+  "Drake",
+];
+const arrayAlbumID = [
+  69874, 35475, 98745, 1596, 18475, 19587, 57250, 25874, 58746, 5874, 487659, 58746, 65874, 8452, 9587, 8540, 7677,
+  2587,
+];
+
 // funzione per mandare saluto in base all'orario
 
 const greetings = () => {
@@ -17,20 +32,38 @@ const greetings = () => {
 
 greetings(); //inovcazione funzione saluto
 
-const arrArtist = ["Eminem", "Rihanna", "The Beatles", "Taylor Swift"];
-const arrayAlbumID = [
-  69874, 35475, 98745, 1596, 18475, 19587, 57250, 25874, 58746, 5874, 487659, 58746, 65874, 8452, 9587, 8540, 7677,
-  2587,
-];
+// funzione per chiusura bottone a destra
+document.addEventListener("DOMContentLoaded", function () {
+  const closeButton = document.getElementById("close-right-sidebar");
+  const contentContainer = document.getElementById("right-sidebar");
+  const colRightSidebar = document.getElementById("col-right-sidebar");
 
+  closeButton.addEventListener("click", function () {
+    contentContainer.classList.add("d-none");
+    colRightSidebar.classList.add("col-1");
+    colRightSidebar.classList.remove("col-2");
+    showButton.classList.remove("d-none");
+  });
+
+  const showButton = document.getElementById("btn-right-sidebar");
+  showButton.addEventListener("click", function () {
+    contentContainer.classList.remove("d-none");
+
+    colRightSidebar.classList.remove("col-1");
+    colRightSidebar.classList.add("col-2");
+
+    showButton.classList.add("d-none");
+  });
+});
+// funzione che randomizza indice in base alla lunghezza dell'array che riceve come parametro
 const indexRandom = (array) => {
   const index = Math.floor(Math.random() * array.length);
-  console.log(index);
   return index;
 };
-console.log(indexRandom(arrayAlbumID));
-// const indexRandom = Math.floor(Math.random() * 4);
-fetch(URL + search + arrArtist[indexRandom(arrArtist)], options)
+
+const UrlArtist = URL + search + arrArtist[indexRandom(arrArtist)];
+console.log(UrlArtist);
+fetch(UrlArtist, options)
   .then((response) => {
     if (!response.ok) {
       throw new Error("Error" + response.status);
@@ -40,7 +73,6 @@ fetch(URL + search + arrArtist[indexRandom(arrArtist)], options)
   })
   .then((data) => {
     const selection = data.data[0];
-    // console.log(selection);
 
     const img = document.getElementById("albumImg");
     const title = document.getElementById("albumTitle");
@@ -75,17 +107,10 @@ fetch(URL + search + arrArtist[indexRandom(arrArtist)], options)
 //     console.log("error" + Error);
 //   });
 const div = document.querySelectorAll(".playlist");
-<<<<<<< HEAD
-div.forEach((element, indexofArray) => {
-  //   console.log(playlist.childNodes);
-  const divImg = document.querySelectorAll(".playlist img")[indexofArray];
-  const divText = document.querySelectorAll(".playlist div p")[indexofArray];
-=======
 div.forEach((element) => {
   //   console.log(playlist.childNodes);
   const divImg = element.querySelector(" img");
   const divText = element.querySelector(" p");
->>>>>>> develop-js
   //   console.log(divText);
   fetch(URL + playlist + arrayAlbumID[indexRandom(arrayAlbumID)], options)
     .then((response) => {

0c63ee03c710c882fdd0bd929c4bfabd16d42920
HachimEnnaji <ashennaji@hotmail.it>
2023-12-13 19:12:39 +0100

    19.11 13/12 popolata pagina artista

--- js/artist.js
+++ js/artist.js
@@ -1,5 +1,35 @@
 import { URL, token, options, search, playlist } from "./token.js";
 const params = new URLSearchParams(window.location.search);
-
 const paramsid = params.has("id") ? params.get("id") : params.get("q");
 console.log(paramsid);
+
+const randomNumber = () => {
+  let n = Math.floor(Math.random() * 1000000 + 2000000);
+  return n;
+};
+fetch(URL + search + paramsid, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Errore di tipo:" + response.status);
+    } else {
+      return response.json();
+    }
+  })
+  .then((object) => {
+    const artist = object.data[0];
+    const artistContainer = document.getElementById("artist");
+    let rank = document.getElementById("rank");
+    const background = document.getElementById("background");
+    background.style.backgroundImage = `URL(${artist.artist.picture_xl})`;
+
+    background.style.objectFit = "contain";
+    background.style.backgroundPositionY = "20%";
+    background.style.backgroundPositionX = "center";
+    rank.innerHTML = randomNumber().toLocaleString();
+    console.log(rank);
+    artistContainer.innerHTML = artist.artist.name;
+    console.log(artistContainer);
+  })
+  .catch((error) => {
+    console.log("errore");
+  });
--- js/token.js
+++ js/token.js
@@ -6,7 +6,7 @@ const options = {
   method: "GET",
   url: URL,
   headers: {
-    "X-RapidAPI-Key": "8f76b272f2msh3aee6aa14119e3bp1a1f1bjsnf563f456d57b",
+    "X-RapidAPI-Key": token,
     "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
   },
 };
